7c262f4ccac514c0ab551912b3c79ec3d7648b76
hoang <hoang@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-05-08 20:00:30 +0000

    Add page creation section.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/jobs-plus@14264 f53fe46e-8c02-459b-9fdd-93e75771b278

--- class/class-admin.php
+++ class/class-admin.php
@@ -1,136 +1,171 @@
 <?php
 /**
-* @package Jobs +
-* @author Arnold Bailey
-* @since version 1.0
-* @license GPL2+
-*/
+ * @package Jobs +
+ * @author  Arnold Bailey
+ * @since   version 1.0
+ * @license GPL2+
+ */
 
-if( !class_exists('Jobs_Plus_Admin') ):
+if ( ! class_exists( 'Jobs_Plus_Admin' ) ):
 
-class Jobs_Plus_Admin extends Jobs_Plus_Core{
+	class Jobs_Plus_Admin extends Jobs_Plus_Core {
 
-	public $menu_page = null;
+		public $menu_page = null;
 
-	function __construct(){
-		parent::__construct();
+		function __construct() {
+			parent::__construct();
 
-		add_action('init', array(&$this, 'on_init') );
-		add_action('admin_menu', array(&$this, 'on_admin_menu'));
-		//add_action('admin_enqueue_scripts', array(&$this, 'on_enqueue_scripts'), 1000 );
-		add_action('admin_enqueue_scripts', array(&$this, 'on_admin_enqueue_scripts') );
-		add_action('admin_enqueue_scripts', array(&$this,'wp_pointer_load'));
-		
-		add_action( 'personal_options', array( &$this, 'on_personal_options' ) );
-		add_action( 'personal_options_update', array( &$this, 'on_edit_user_profile_update' ) );
-		add_action( 'edit_user_profile_update', array( &$this, 'on_edit_user_profile_update' ) );
+			add_action( 'init', array( &$this, 'on_init' ) );
+			add_action( 'admin_menu', array( &$this, 'on_admin_menu' ) );
+			//add_action('admin_enqueue_scripts', array(&$this, 'on_enqueue_scripts'), 1000 );
+			add_action( 'admin_enqueue_scripts', array( &$this, 'on_admin_enqueue_scripts' ) );
+			add_action( 'admin_enqueue_scripts', array( &$this, 'wp_pointer_load' ) );
 
-	}
+			add_action( 'personal_options', array( &$this, 'on_personal_options' ) );
+			add_action( 'personal_options_update', array( &$this, 'on_edit_user_profile_update' ) );
+			add_action( 'edit_user_profile_update', array( &$this, 'on_edit_user_profile_update' ) );
+			//add action for handler the page creation
+			add_action( 'init', array( $this, 'create_virtual_pages' ), 20 );
 
-	function on_init(){
-		$this->handle_post();
-		//Now init
-		parent::on_init();
-	}
-
-	function on_admin_menu( ){
-		$this->jobs_menu_page = add_submenu_page('edit.php?post_type=jbp_job',
-		__('Settings', JBP_TEXT_DOMAIN),
-		__('Settings', JBP_TEXT_DOMAIN),
-		'manage_options', 'jobs-plus-menu',
-		array($this, 'admin_menu_page_job'),
-		$this->plugin_url . 'img/jobs-plus-16.png' );
-
-		$this->pros_menu_page = add_submenu_page('edit.php?post_type=jbp_pro',
-		__('Settings', JBP_TEXT_DOMAIN),
-		__('Settings', JBP_TEXT_DOMAIN),
-		'manage_options', 'jobs-plus-menu',
-		array($this, 'admin_menu_page_pro'),
-		$this->plugin_url . 'img/jobs-plus-16.png' );
-
-		add_action('load-' . $this->jobs_menu_page, array(&$this, 'on_load_menu') );
-		add_action('load-' . $this->pros_menu_page, array(&$this, 'on_load_menu') );
-	}
-
-	function admin_menu_page_job(){
-		$current_tab = (empty($_GET['tab']) )? 'settings' : $_GET['tab'];
-
-		switch ($current_tab) {
-			case 'settings': include $this->plugin_dir . 'ui-admin/settings.php'; break;
-			case 'job': include $this->plugin_dir . 'ui-admin/job.php'; break;
-			case 'pro': include $this->plugin_dir . 'ui-admin/pro.php';	break;
-			case 'shortcodes': include $this->plugin_dir . 'ui-admin/shortcodes.php';	break;
-			case 'convert': include $this->plugin_dir . 'ui-admin/convert.php';	break;
-			default: include $this->plugin_dir . 'ui-admin/job.php'; break;
-		}
-	}
-
-	function admin_menu_page_pro(){
-		$current_tab = (empty($_GET['tab']) )? 'pro' : $_GET['tab'];
-
-		switch ($current_tab) {
-			case 'settings': include $this->plugin_dir . 'ui-admin/settings.php'; break;
-			case 'job': include $this->plugin_dir . 'ui-admin/job.php'; break;
-			case 'pro': include $this->plugin_dir . 'ui-admin/pro.php';	break;
-			case 'shortcodes': include $this->plugin_dir . 'ui-admin/shortcodes.php';	break;
-			case 'convert': include $this->plugin_dir . 'ui-admin/convert.php';	break;
-			default: include $this->plugin_dir . 'ui-admin/job.php'; break;
-		}
-	}
-
-	function on_admin_enqueue_scripts	(){
-		wp_enqueue_style('jobs-plus-admin-css', $this->plugin_url . 'css/jobs-plus-admin.css');
-	}
-
-	function on_load_menu(){
-
-	}
-
-	function render_tabs( $current_tab = 'settings'){
-		$tabs = array(
-		'settings' => __('General', JBP_TEXT_DOMAIN),
-		'job' => sprintf(__('%s Options', JBP_TEXT_DOMAIN), $this->job_labels->singular_name),
-		'pro' => sprintf(__('%s Options', JBP_TEXT_DOMAIN), $this->pro_labels->singular_name),
-		'shortcodes' => __('Shortcodes', JBP_TEXT_DOMAIN),
-		//'convert' => __('Convert from WPMU Jobs', JBP_TEXT_DOMAIN),
-		);
-
-		$current_tab = (empty($_GET['tab']) )? $current_tab : $_GET['tab'];
-
-		?>
-		<h2 class="nav-tab-wrapper">
-			<?php foreach( $tabs as $tab => $title):
-			$class = ($tab === $current_tab) ? 'nav-tab-active' : '';
-			?>
-			<a class="nav-tab <?php echo $class ?>" href="<?php esc_attr_e("?post_type=jbp_job&page=jobs-plus-menu&tab=$tab"); ?>" ><img src="<?php echo $this->plugin_url . "img/{$tab}16.png";?>" /> <?php echo $title; ?></a>
-				<?php endforeach; ?>
-			</h2>
-
-			<?php
 		}
 
-		/**
-		* Handle posts from the various jobs board menus
-		*
-		*/
-		function handle_post(){
+		function on_init() {
+			$this->handle_post();
+			//Now init
+			parent::on_init();
+		}
 
-			if( defined('DOING_AJAX') || empty($_POST['jobs-plus-settings']) ) return;
+		function on_admin_menu() {
+			$this->jobs_menu_page = add_submenu_page( 'edit.php?post_type=jbp_job',
+				__( 'Settings', JBP_TEXT_DOMAIN ),
+				__( 'Settings', JBP_TEXT_DOMAIN ),
+				'manage_options', 'jobs-plus-menu',
+				array( $this, 'admin_menu_page_job' ),
+				$this->plugin_url . 'img/jobs-plus-16.png' );
 
-			check_admin_referer('jobs-plus-settings');
+			$this->pros_menu_page = add_submenu_page( 'edit.php?post_type=jbp_pro',
+				__( 'Settings', JBP_TEXT_DOMAIN ),
+				__( 'Settings', JBP_TEXT_DOMAIN ),
+				'manage_options', 'jobs-plus-menu',
+				array( $this, 'admin_menu_page_pro' ),
+				$this->plugin_url . 'img/jobs-plus-16.png' );
 
-			$params = stripslashes_deep($_POST);
+			add_action( 'load-' . $this->jobs_menu_page, array( &$this, 'on_load_menu' ) );
+			add_action( 'load-' . $this->pros_menu_page, array( &$this, 'on_load_menu' ) );
+		}
 
-			if(! empty($params['jbp']) ) {
-				$settings = get_option($this->settings_name);
-				$settings = array_replace((array)$settings, $params['jbp'] );
-				update_option($this->settings_name, $settings);
+		function admin_menu_page_job() {
+			$current_tab = ( empty( $_GET['tab'] ) ) ? 'settings' : $_GET['tab'];
+
+			switch ( $current_tab ) {
+				case 'settings':
+					include $this->plugin_dir . 'ui-admin/settings.php';
+					break;
+				case 'job':
+					include $this->plugin_dir . 'ui-admin/job.php';
+					break;
+				case 'pro':
+					include $this->plugin_dir . 'ui-admin/pro.php';
+					break;
+				case 'shortcodes':
+					include $this->plugin_dir . 'ui-admin/shortcodes.php';
+					break;
+				case 'convert':
+					include $this->plugin_dir . 'ui-admin/convert.php';
+					break;
+				case 'page_creation':
+					include $this->plugin_dir . 'ui-admin/page_creation.php';
+					break;
+				default:
+					include $this->plugin_dir . 'ui-admin/job.php';
+					break;
 			}
 		}
 
-		function on_personal_options($profileuser = 0){
-			if ( !current_user_can( 'promote_users') || !$this->get_setting('general->use_certification') ) return false;
-			$certification = $this->get_setting('general->certification', __('Jobs + Certified', JBP_TEXT_DOMAIN) );
+		function admin_menu_page_pro() {
+			$current_tab = ( empty( $_GET['tab'] ) ) ? 'pro' : $_GET['tab'];
+
+			switch ( $current_tab ) {
+				case 'settings':
+					include $this->plugin_dir . 'ui-admin/settings.php';
+					break;
+				case 'job':
+					include $this->plugin_dir . 'ui-admin/job.php';
+					break;
+				case 'pro':
+					include $this->plugin_dir . 'ui-admin/pro.php';
+					break;
+				case 'shortcodes':
+					include $this->plugin_dir . 'ui-admin/shortcodes.php';
+					break;
+				case 'convert':
+					include $this->plugin_dir . 'ui-admin/convert.php';
+					break;
+				default:
+					include $this->plugin_dir . 'ui-admin/job.php';
+					break;
+			}
+		}
+
+		function on_admin_enqueue_scripts() {
+			wp_enqueue_style( 'jobs-plus-admin-css', $this->plugin_url . 'css/jobs-plus-admin.css' );
+		}
+
+		function on_load_menu() {
+
+		}
+
+		function render_tabs( $current_tab = 'settings' ) {
+			$tabs = array(
+				'settings'      => __( 'General', JBP_TEXT_DOMAIN ),
+				'job'           => sprintf( __( '%s Options', JBP_TEXT_DOMAIN ), $this->job_labels->singular_name ),
+				'pro'           => sprintf( __( '%s Options', JBP_TEXT_DOMAIN ), $this->pro_labels->singular_name ),
+				'shortcodes'    => __( 'Shortcodes', JBP_TEXT_DOMAIN ),
+				'page_creation' => __( 'Page Creation', JBP_TEXT_DOMAIN ),
+				//'convert' => __('Convert from WPMU Jobs', JBP_TEXT_DOMAIN),
+			);
+
+			$current_tab = ( empty( $_GET['tab'] ) ) ? $current_tab : $_GET['tab'];
+
+			?>
+			<h2 class="nav-tab-wrapper">
+				<?php foreach ( $tabs as $tab => $title ):
+					$class = ( $tab === $current_tab ) ? 'nav-tab-active' : '';
+					?>
+					<a class="nav-tab <?php echo $class ?>" href="<?php esc_attr_e( "?post_type=jbp_job&page=jobs-plus-menu&tab=$tab" ); ?>"><img src="<?php echo $this->plugin_url . "img/{$tab}16.png"; ?>" /> <?php echo $title; ?>
+					</a>
+				<?php endforeach; ?>
+			</h2>
+
+		<?php
+		}
+
+		/**
+		 * Handle posts from the various jobs board menus
+		 *
+		 */
+		function handle_post() {
+
+			if ( defined( 'DOING_AJAX' ) || empty( $_POST['jobs-plus-settings'] ) ) {
+				return;
+			}
+
+			check_admin_referer( 'jobs-plus-settings' );
+
+			$params = stripslashes_deep( $_POST );
+
+			if ( ! empty( $params['jbp'] ) ) {
+				$settings = get_option( $this->settings_name );
+				$settings = array_replace( (array) $settings, $params['jbp'] );
+				update_option( $this->settings_name, $settings );
+			}
+		}
+
+		function on_personal_options( $profileuser = 0 ) {
+			if ( ! current_user_can( 'promote_users' ) || ! $this->get_setting( 'general->use_certification' ) ) {
+				return false;
+			}
+			$certification = $this->get_setting( 'general->certification', __( 'Jobs + Certified', JBP_TEXT_DOMAIN ) );
 			?>
 			<tr class="jbp-certified">
 				<th scope="row"><?php echo $certification; ?></th>
@@ -139,81 +174,145 @@ class Jobs_Plus_Admin extends Jobs_Plus_Core{
 						<legend class="screen-reader-text"><span><?php echo $certification; ?></span></legend>
 						<label for="jbp_certified">
 							<input name="jbp_certified" type="checkbox" id="jbp_certified" value="1"<?php checked( is_jbp_certified( $profileuser->ID ) ); ?> />
-							<?php printf( __('User is %s', JBP_TEXT_DOMAIN ), $certification); ?>
+							<?php printf( __( 'User is %s', JBP_TEXT_DOMAIN ), $certification ); ?>
 						</label><br />
 					</fieldset>
 				</td>
 			</tr>
-			<?php
+		<?php
 		}
 
-		function on_edit_user_profile_update($user_id = 0){
-			if ( !current_user_can( 'promote_users') || !$this->get_setting('general->use_certification') ) return false;
+		function on_edit_user_profile_update( $user_id = 0 ) {
+			if ( ! current_user_can( 'promote_users' ) || ! $this->get_setting( 'general->use_certification' ) ) {
+				return false;
+			}
 
-			update_user_meta($user_id, JBP_PRO_CERTIFIED_KEY, $_POST['jbp_certified']);
+			update_user_meta( $user_id, JBP_PRO_CERTIFIED_KEY, $_POST['jbp_certified'] );
 		}
 
-	/**
-	* wp_pointer_load - Loads the WordPress tips pointers for Jobs.
-	*
-	*/
-	function wp_pointer_load(){
+		/**
+		 * wp_pointer_load - Loads the WordPress tips pointers for Jobs.
+		 *
+		 */
+		function wp_pointer_load() {
 
-		//var_dump(get_current_screen()->id);
+			//var_dump(get_current_screen()->id);
 
-		$cookie_content = __('<p>WHMCS WordPress Integration can now sync certain cookies between WHMCS and Wordpress so that downloads of protected files from WHMCS can work correctly in WordPress.</p> <p>This requires copying the "wp-integration.php" file in this plugin to the root of the WHMCS System installation.</p>', JBP_TEXT_DOMAIN);
+			$cookie_content = __( '<p>WHMCS WordPress Integration can now sync certain cookies between WHMCS and Wordpress so that downloads of protected files from WHMCS can work correctly in WordPress.</p> <p>This requires copying the "wp-integration.php" file in this plugin to the root of the WHMCS System installation.</p>', JBP_TEXT_DOMAIN );
 
-		//Setup any new feature notices
-		include $this->plugin_dir . 'class/class-wp-help-pointers.php';
-		
-		$pointers = array(
-		array(
-		'id' => 'wcp_endpoint',   // unique id for this pointer
-		'screen' => 'toplevel_page_wcp-settings', // this is the page hook we want our pointer to show on
-		'target' => '#wcp-endpoint', // the css selector for the pointer to be tied to, best to use ID's
-		'title' => __('NEW - Permalinks Endpoint Slug', JBP_TEXT_DOMAIN),
-		'content' => __('<p>This is the slug that signals that the following page is to be pulled from the WHMCS site.</p> <p>You can change it to whatever you like to avoid interfering with other pages but like all slugs it should contain Only lowercase alphanumerics and the hyphen.</p>', JBP_TEXT_DOMAIN),
-		'position' => array(
-		'edge' => 'top', //top, bottom, left, right
-		'align' => 'middle' //top, bottom, left, right, middle
-		)
-		),
+			//Setup any new feature notices
+			include $this->plugin_dir . 'class/class-wp-help-pointers.php';
 
-		array(
-		'id' => 'wcp_cookies',   // unique id for this pointer
-		'screen' => 'plugins', // this is the page hook we want our pointer to show on
-		'target' => '#toplevel_page_wcp-settings', // the css selector for the pointer to be tied to, best to use ID's
-		'title' => __('NEW - WHMCS WordPress Integration Cookie syncing', JBP_TEXT_DOMAIN),
-		'content' => $cookie_content,
-		'position' => array(
-		'edge' => 'left', //top, bottom, left, right
-		'align' => 'right' //top, bottom, left, right, middle
-		)
-		),
+			$pointers = array(
+				array(
+					'id'       => 'wcp_endpoint', // unique id for this pointer
+					'screen'   => 'toplevel_page_wcp-settings', // this is the page hook we want our pointer to show on
+					'target'   => '#wcp-endpoint', // the css selector for the pointer to be tied to, best to use ID's
+					'title'    => __( 'NEW - Permalinks Endpoint Slug', JBP_TEXT_DOMAIN ),
+					'content'  => __( '<p>This is the slug that signals that the following page is to be pulled from the WHMCS site.</p> <p>You can change it to whatever you like to avoid interfering with other pages but like all slugs it should contain Only lowercase alphanumerics and the hyphen.</p>', JBP_TEXT_DOMAIN ),
+					'position' => array(
+						'edge'  => 'top', //top, bottom, left, right
+						'align' => 'middle' //top, bottom, left, right, middle
+					)
+				),
 
-		array(
-		'id' => 'wcp_cookies',   // unique id for this pointer
-		'screen' => 'toplevel_page_wcp-settings', // this is the page hook we want our pointer to show on
-		'target' => '#toplevel_page_wcp-settings', // the css selector for the pointer to be tied to, best to use ID's
-		'title' => __('NEW - WHMCS WordPress Integration Cookie syncing', JBP_TEXT_DOMAIN),
-		'content' => $cookie_content,
-		'position' => array(
-		'edge' => 'left', //top, bottom, left, right
-		'align' => 'right' //top, bottom, left, right, middle
-		)
-		),
+				array(
+					'id'       => 'wcp_cookies', // unique id for this pointer
+					'screen'   => 'plugins', // this is the page hook we want our pointer to show on
+					'target'   => '#toplevel_page_wcp-settings', // the css selector for the pointer to be tied to, best to use ID's
+					'title'    => __( 'NEW - WHMCS WordPress Integration Cookie syncing', JBP_TEXT_DOMAIN ),
+					'content'  => $cookie_content,
+					'position' => array(
+						'edge'  => 'left', //top, bottom, left, right
+						'align' => 'right' //top, bottom, left, right, middle
+					)
+				),
 
-		// more as needed
-		);
+				array(
+					'id'       => 'wcp_cookies', // unique id for this pointer
+					'screen'   => 'toplevel_page_wcp-settings', // this is the page hook we want our pointer to show on
+					'target'   => '#toplevel_page_wcp-settings', // the css selector for the pointer to be tied to, best to use ID's
+					'title'    => __( 'NEW - WHMCS WordPress Integration Cookie syncing', JBP_TEXT_DOMAIN ),
+					'content'  => $cookie_content,
+					'position' => array(
+						'edge'  => 'left', //top, bottom, left, right
+						'align' => 'right' //top, bottom, left, right, middle
+					)
+				),
 
-		//new WP_Help_Pointer($pointers);
+				// more as needed
+			);
+			//new WP_Help_Pointer($pointers);
+		}
+
+		//create virtual page
+		function create_virtual_pages() {
+			$action = isset( $_GET['action'] ) ? $_GET['action'] : null;
+			if ( ! empty( $action ) ) {
+				$tab_url = 'edit.php?post_type=' . $_GET['post_type'] . '&page=' . $_GET['page'] . '&tab=' . $_GET['tab'];
+
+				$post_content = __( "<p>Virtual page. Editing this page won\'t change anything.<br />", JBP_TEXT_DOMAIN );
+				$post_content .= __( "You may edit the Title and/or the slug only.</p>", JBP_TEXT_DOMAIN );
+
+				switch ( $action ) {
+					case 'jbp_create_add_job_page':
+						check_admin_referer( 'jbp_create_add_job_page' );
+						//get the post type info
+						$post_type = get_post_type_object( 'jbp_job' );
+						$args      = array(
+							'post_title'     => sprintf( 'Add %s', $post_type->labels->singular_name ),
+							'post_name'      => sprintf( 'add-%s', $post_type->rewrite['slug'] ),
+							'post_status'    => 'virtual',
+							'post_author'    => get_current_user_id(),
+							'post_type'      => 'jbp_job',
+							'post_content'   => $post_content,
+							'ping_status'    => 'closed',
+							'comment_status' => 'closed'
+						);
+						$id        = wp_insert_post( $args );
+						wp_safe_redirect( admin_url( $tab_url . '&add_job_page_id=' . $id ) );
+						break;
+					case 'jbp_create_add_pro_page':
+						check_admin_referer( 'jbp_create_add_pro_page' );
+						$post_type = get_post_type_object( 'jbp_pro' );
+						$args      = array(
+							'post_title'     => sprintf( 'Add %s', $post_type->labels->singular_name ),
+							'post_name'      => sprintf( 'add-%s', $post_type->rewrite['slug'] ),
+							'post_status'    => 'virtual',
+							'post_author'    => get_current_user_id(),
+							'post_type'      => 'jbp_pro',
+							'post_content'   => $post_content,
+							'ping_status'    => 'closed',
+							'comment_status' => 'closed'
+						);
+						$id        = wp_insert_post( $args );
+						wp_safe_redirect( admin_url( $tab_url . '&jbp_add_pro_page_id=' . $id ) );
+						break;
+					case 'jbp_create_full_page':
+						check_admin_referer( 'jbp_create_full_page' );
+						if ( $this->get_setting( 'pages_define->job_board', false ) === false || get_post($this->get_setting( 'pages_define->job_board')) == null) {
+
+							$post_content = '[jbp-job-post-btn text="Post a Job" view="both"][jbp-job-browse-btn text="Find a Pro" view="both"][jbp-pro-browse-btn text="Browse Jobs" view="both"][jbp-pro-post-btn text="Become a Pro" view="both"]';
+							$args         = array(
+								'post_title'     => sprintf( 'Job board' ),
+								'post_status'    => 'publish',
+								'post_author'    => get_current_user_id(),
+								'post_type'      => 'page',
+								'post_content'   => $post_content,
+								'ping_status'    => 'closed',
+								'comment_status' => 'closed'
+							);
+							$id           = wp_insert_post( $args );
+							//update new page to setting
+							$settings                              = $settings = get_option( $this->settings_name );
+							$settings['pages_define']['job_board'] = $id;
+							update_option( $this->settings_name, $settings );
+						}
+						wp_safe_redirect( $tab_url );
+				}
+			}
+		}
+
 	}
 
-
-
-
-
-
-	}
-
-	endif;
+endif;
