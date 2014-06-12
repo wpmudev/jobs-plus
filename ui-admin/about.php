7bb78321c208a8b46376eaeb3f2c1eb3f9067ed5
Arnold Bailey <abaileytn@gmai.com>
2014-06-11 20:36:44 -0400

    Getting Started page touchup

--- ui-admin/about.php
+++ ui-admin/about.php
@@ -24,18 +24,18 @@ wp_enqueue_style( 'magnific-popup' );
 
 	<div class="jbp_plans">
 		<div class="jbp_plan">
-			<p class="first"><a href="<?php echo get_post_type_archive_link('jbp_job'); ?>" class="jbp_button jbp_job_pages">Jobs Page</a></p>
-			<p><a href="<?php echo admin_url('post.php?post='.$this->job_archive_page_id.'&action=edit') ?>"><?php _e( 'Edit', JBP_TEXT_DOMAIN ) ?></a> <?php _e( ' this pattern', JBP_TEXT_DOMAIN ) ?></p>
-			<p><a href="<?php echo get_permalink($this->job_update_page_id) ?>"><?php _e( 'Add', JBP_TEXT_DOMAIN ) ?></a> <?php _e( 'a new job', JBP_TEXT_DOMAIN ) ?> </p>
+			<p class="first"><a href="<?php echo esc_attr( get_post_type_archive_link('jbp_job') ); ?>" class="jbp_button jbp_job_pages"><?php echo esc_html( sprintf(__('%s Page', JBP_TEXT_DOMAIN), $this->job_labels->name) );?></a></p>
+			<p><a href="<?php echo esc_attr( admin_url('post.php?post='.$this->job_archive_page_id.'&action=edit') );?>"><?php _e( 'Edit', JBP_TEXT_DOMAIN ) ?></a><?php esc_html_e( ' this pattern', JBP_TEXT_DOMAIN ) ?></p>
+			<p><a href="<?php echo esc_attr( get_permalink($this->job_update_page_id) );?>"><?php _e( 'Add', JBP_TEXT_DOMAIN ) ?></a> <?php echo esc_html( sprintf( __('a new %s', JBP_TEXT_DOMAIN ), $this->job_labels->singular_name ) ); ?></p>
 		</div>
 		<div class="jbp_plan">
-			<p class="first"><a href="<?php echo get_permalink($this->demo_landing_page_id) ?>" class="jbp_button jbp_landing_page">Landing Page</a></p>
-			<p><a href="<?php echo admin_url('post.php?post='.$this->demo_landing_page_id.'&action=edit') ?>"><?php _e( 'Edit', JBP_TEXT_DOMAIN ) ?></a> <?php _e( ' this pattern', JBP_TEXT_DOMAIN ) ?></p>
+			<p class="first"><a href="<?php echo esc_attr( get_permalink($this->demo_landing_page_id) );?>" class="jbp_button jbp_landing_page"><?php esc_html_e('Landing Page',JBP_TEXT_DOMAIN );?></a></p>
+			<p><a href="<?php echo esc_attr( admin_url('post.php?post='.$this->demo_landing_page_id.'&action=edit') );?>"><?php esc_html_e('Edit', JBP_TEXT_DOMAIN ); ?></a> <?php esc_html_e( ' this pattern', JBP_TEXT_DOMAIN ) ?></p>
 		</div>
 		<div class="jbp_plan">
-			<p class="first"><a href="<?php echo get_post_type_archive_link('jbp_pro'); ?>" class="jbp_button jbp_experts_pages">Experts Page</a></p>
-			<p><a href="<?php echo admin_url('post.php?post='.$this->pro_archive_page_id.'&action=edit') ?>"><?php _e( 'Edit', JBP_TEXT_DOMAIN ) ?></a> <?php _e( ' this pattern', JBP_TEXT_DOMAIN ) ?></p>
-			<p><a href="<?php echo get_permalink($this->pro_update_page_id) ?>"><?php _e( 'Add', JBP_TEXT_DOMAIN ) ?></a> <?php _e( 'a new expert', JBP_TEXT_DOMAIN ) ?> </p>
+			<p class="first"><a href="<?php echo esc_attr( get_post_type_archive_link('jbp_pro') ); ?>" class="jbp_button jbp_experts_pages"><?php echo esc_html( sprintf(__('%s Page', JBP_TEXT_DOMAIN), $this->pro_labels->name) );?></a></p>
+			<p><a href="<?php echo esc_attr( admin_url('post.php?post='.$this->pro_archive_page_id.'&action=edit') ); ?>"><?php _e( 'Edit', JBP_TEXT_DOMAIN ) ?></a> <?php _e( ' this pattern', JBP_TEXT_DOMAIN ) ?></p>
+			<p><a href="<?php echo esc_attr( get_permalink($this->pro_update_page_id) ); ?>"><?php esc_html_e( 'Add', JBP_TEXT_DOMAIN ) ?></a> <?php echo esc_html( sprintf( __('a new %s', JBP_TEXT_DOMAIN ), $this->pro_labels->singular_name ) ); ?> </p>
 		</div>
 	</div>
 </div>
