0e1f3810a03c9a6035f221a9447c1008633c744f
HoangNgo <hoang@incsub.com>
2014-12-29 14:41:42 +0700

    Update version + change log

--- jobs-experts.php
+++ jobs-experts.php
@@ -3,7 +3,7 @@
  * Plugin Name: Jobs and Experts
  * Plugin URI: http://premium.wpmudev.org/jobs-plus/
  * Description: Match people with projects to industry professionals – it’s more than your average WordPress jobs board.
- * Version: 1.0.1.4
+ * Version: 1.0.1.5
  * Author:WPMU DEV
  * Author URI: http://premium.wpmudev.org
  * Text Domain: jbp
