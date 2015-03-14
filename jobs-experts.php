d3b137e68f050bd26b7c182c096d440c4c64bf3d
HoangNgo <hoang@incsub.com>
2015-03-14 14:33:14 +0700

    Add function to check user can upload avatar

--- jobs-experts.php
+++ jobs-experts.php
@@ -382,6 +382,29 @@ class Jobs_Experts
         return false;
     }
 
+    function can_upload_avatar()
+    {
+        if (!is_user_logged_in()) {
+            return false;
+        }
+
+        if (current_user_can('upload_files'))
+            return true;
+
+        $allowed = $this->settings()->allow_avatar;
+        if (!is_array($allowed)) {
+            $allowed = array();
+        }
+        $allowed = array_filter($allowed);
+        $user = new WP_User(get_current_user_id());
+        foreach ($user->roles as $role) {
+            if (in_array($role, $allowed)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     function autoload($class)
     {
         $filename = str_replace('_', '-', strtolower($class)) . '.php';
