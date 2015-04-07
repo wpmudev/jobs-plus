57f7b5725203bfb9d6be254423b3503e91c1639b
HoangNgo <hoang@incsub.com>
2015-04-07 15:47:33 +0700

    Show admin notice if the addon MP integration enabled without marketpress

--- app/addons/je-marketpress.php
+++ app/addons/je-marketpress.php
@@ -14,11 +14,33 @@ class JE_MarketPress
         if (is_object($mp) && $mp instanceof MarketPress) {
             include_once(je()->plugin_path . 'app/components/ig-wallet.php');
             include_once(je()->plugin_path . 'app/components/je-credit-rules.php');
+        } else {
+            /**
+             * this is the case if the add-on activated, but admin deactivated MP plugin
+             * @since 1.0.1.9
+             */
+            $settings = je()->settings()->plugins;
+            if (in_array(__FILE__, $settings)) {
+                //add an admin notice
+                add_action('admin_notices', array(&$this, 'admin_notices'));
+            }
         }
+
 
         //disabled all the product
         add_action('je_addon_activated', array(&$this, 'active'), 10, 2);
         add_action('je_addon_deactivated', array(&$this, 'clean_up'), 10, 2);
+    }
+
+    /**
+     * Show admin notice if the addon enabled without marketpress
+     * @since 1.0.1.9
+     */
+    function admin_notices()
+    {
+        $class = "error";
+        $message = __("The add-on <strong>MarketPress integration</strong> of <strong>Jobs&Experts</strong> required <strong>MarketPress</strong>", je()->domain);
+        echo "<div class=\"$class\"> <p>$message</p></div>";
     }
 
     function active($id, $meta)
