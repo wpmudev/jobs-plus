f709d5fd84ca25d348f4c7632ff867d3084c103b
Xenos Konstantinos <mr.xkon@gmail.com>
2018-08-02 12:45:06 +0300

    suggested policy text
    suggested policy text

--- app/controllers/backend/policy-text.php
+++ app/controllers/backend/policy-text.php
@@ -1,20 +1,28 @@
 <div class="wp-suggested-text">
 	<h2><?php esc_html_e( 'What personal data do we collect and why?', je()->domain ); ?></h2>
 	<p class="privacy-policy-tutorial">
-		<?php _e( 'Jobs & Experts keeps the data submitted through either the Jobs or Experts new listing forms as well as the <strong>IP Address</strong> of visitors and members when viewing a Listing to retain and count the correct Views per listing.', je()->domain ); ?>
+		<?php _e( 'Jobs & Experts keeps the data submitted through either the Jobs or Experts new listing forms as well as an <strong>encrypted IP Address</strong> of visitors and members when viewing a Listing to retain and count the correct Views per listing.', je()->domain ); ?>
+	</p>
+	<p class="privacy-policy-tutorial">
+		<strong><?php _e( 'Full set of data gathered for Jobs:', je()->domain ); ?></strong><br/>
+		<?php _e( 'Title, Description, Email, Skills, Budget, Expiration Date, Open Days', je()->domain ); ?>
+	</p>
+	<p class="privacy-policy-tutorial">
+		<strong><?php _e( 'Full set of data gathered for Experts:', je()->domain ); ?></strong><br/>
+		<?php _e( 'Name, Biography, Tag Line, Company, Location, Email, Skills, Social Profiles', je()->domain ); ?>
 	</p>
 	<p>
 		<strong class="privacy-policy-tutorial"><?php esc_html_e( 'Suggested text: ', je()->domain ); ?></strong>
-		<?php _e( 'When visitors or members view our Listings the <strong>IP Address</strong> is kept for statistical purposes.', je()->domain );  ?>
+		<?php _e( 'When visitors or members view our Listings the IP Address is encrypted and kept for statistical purposes. When you submit a Job or an Expert all data filled in the forms are gathered and kept until deletion.', je()->domain );  ?>
 	</p>
 
 	<h2><?php esc_html_e( 'How long we retain your data', je()->domain ); ?></h2>
 	<p class="privacy-policy-tutorial">
-		<?php _e( 'Until the listings are deleted by either the Author, the Administrator or a Data Erasure request.', je()->domain ); ?>
+		<?php _e( 'Until the listings are deleted.', je()->domain ); ?>
 	</p>
 	<p>
 		<strong class="privacy-policy-tutorial"><?php esc_html_e( 'Suggested text: ', je()->domain ); ?></strong>
-		<?php esc_html_e( 'The data are kept until the listings are deleted by either the Author, the Administrator', je()->domain ); ?>
+		<?php esc_html_e( 'The data are kept until the listings are deleted by either the Author or the Administrator.', je()->domain ); ?>
 	</p>
 
 	<h2><?php esc_html_e( 'Where we send your data', je()->domain ); ?></h2>
