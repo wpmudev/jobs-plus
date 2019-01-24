50925ad77aeaed33d8729a1d9465154ea11c6f99
Konstantinos Xenos <mr.xkon@gmail.com>
2019-01-24 20:00:26 +0200

    fixes same origin error for page manager
    See https://app.asana.com/0/211855939023775/515832741830674

--- app/addons/je-pages-manager.php
+++ app/addons/je-pages-manager.php
@@ -531,7 +531,8 @@ class JE_Pages_Manager
                         },
                         success: function (data) {
                             var element = that.parent().parent().find('select').first();
-                            $.get('<?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ?>', function (html) {
+							<?php $ssl = ( is_ssl() ) ? 'https://' : 'http'; ?>
+                            $.get('<?php echo $ssl . $_SERVER[HTTP_HOST] . $_SERVER[REQUEST_URI]; ?>', function (html) {
                                 html = $(html);
                                 var clone = html.find('select[name="' + element.attr('name') + '"]');
                                 element.replaceWith(clone);
